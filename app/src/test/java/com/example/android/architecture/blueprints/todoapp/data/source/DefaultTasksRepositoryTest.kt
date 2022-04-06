45d0bef35a89c259e7a22c34923266e03c1a31b1
Apolo <apolorossi@gmail.com>
2022-04-05 21:37:04 -0300

    Criado testes na camada do Repository. Importado lib de testes para coroutines.

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -0,0 +1,61 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.core.IsEqual
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [29])
+class DefaultTasksRepositoryTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private val task1 = Task("Title1", "Description1")
+    private val task2 = Task("Title2", "Description2")
+    private val task3 = Task("Title3", "Description3")
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
+    private val localTasks = listOf(task3).sortedBy { it.id }
+    private val newTasks = listOf(task3).sortedBy { it.id }
+
+    lateinit var tasksRemoteDataSource : TasksDataSource
+    lateinit var tasksLocalDataSource : TasksDataSource
+    lateinit var tasksRepository : DefaultTasksRepository
+
+    @Before
+    fun createRepository() {
+        tasksRemoteDataSource = FakeDataSource(remoteTasks.toMutableList())
+        tasksLocalDataSource = FakeDataSource(localTasks.toMutableList())
+        // Get a reference to the class under test
+        tasksRepository = DefaultTasksRepository(
+            // TODO Dispatchers.Unconfined should be replaced with Dispatchers.Main
+            //  this requires understanding more about coroutines + testing
+            //  so we will keep this as Unconfined for now.
+            tasksRemoteDataSource, tasksLocalDataSource, Dispatchers.Unconfined
+        )
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun getTasks_requestsAllTasksFromRemoteDataSource() = runBlockingTest {
+        val tasks = tasksRepository.getTasks(true) as Result.Success
+
+        assertThat(tasks.data, IsEqual(remoteTasks))
+    }
+
+
+
+
+
+}
